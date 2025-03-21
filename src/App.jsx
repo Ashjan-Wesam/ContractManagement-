d7d20de90f70a3a196eacf98096b27b0308327bc
majd alkaseeh <148889739+Majd-oreo@users.noreply.github.com>
2025-03-21 14:39:54 +0300

    jj

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/profile" element={<Profile />} />
-            <Route path="/editProfile" element={<EditUser/>} />
+            <Route path="/profile/editProfile" element={<EditUser/>} />
 
             
             <Route path="/contracts" element={<Contracts />} />
